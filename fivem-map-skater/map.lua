ac9eb85fd9585a56ccc7576a838774848aab634e
Michael Shipley <admin@dotexe.cf>
2020-07-22 16:19:14 -0400

    Add fivem-map-skater with custom spawn points

---
+++ fivem-map-skater/map.lua
@@ -0,0 +1,3 @@
+spawnpoint 'a_m_y_hipster_01' { x = -543.36, y = -891.2, z = 24.83, heading = 180 }
+spawnpoint 'a_m_y_hipster_01' { x = -552.5, y = -891.1, z = 24.98, heading = 180 }
+spawnpoint 'a_m_y_hipster_01' { x = -562.69, y = -890.8, z = 25.01, heading = 180 }
