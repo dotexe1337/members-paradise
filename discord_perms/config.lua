b0b941a45ea862ba21450aa866cae5cb02828cb6
Michael Shipley <admin@dotexe.cf>
2020-08-18 14:54:52 -0400

    Initial discord_perms commit

---
+++ discord_perms/config.lua
@@ -0,0 +1,9 @@
+Config = {
+	DiscordToken = "NDc0NzQ0MjYzNTYxNzA3NTQz.W2OqDA.ne3LgSSqaleviUVBty4q_HRk5xA",
+	GuildId = "678568534674243584",
+
+	-- Format: ["Role Nickname"] = "Role ID" You can get role id by doing \@RoleName
+	Roles = {
+		["Beta Whitelist"] = "745345646625554432" -- This would be checked by doing exports.discord_perms:IsRolePresent(user, "TestRole")
+	}
+}
